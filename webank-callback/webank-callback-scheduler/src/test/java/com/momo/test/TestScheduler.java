3fc04a6906b03e4d0f1e370fe824b5867716489f
mohy <mohy@dtds.com.cn>
2017-12-12 15:27:24 +0800

    合并代码增加分布式JOB:    xxl-job

---
+++ webank-callback/webank-callback-scheduler/src/test/java/com/momo/test/TestScheduler.java
@@ -0,0 +1,15 @@
+package com.momo.test;
+
+/** 
+ * xxl-job
+ * 
+ * http://blog.csdn.net/u013294278/article/details/77414887
+ * https://gitee.com/JornTang 
+ * 
+ * @author <a href="mailto:mohy@dtds.com.cn">莫海涌</a> 
+ * @company 深圳动态网络科技有限公司 版权所有 (c) 2016 
+ * @version 2017年12月12日 
+ */
+public class TestScheduler {
+
+}
